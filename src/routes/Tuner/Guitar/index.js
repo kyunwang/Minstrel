61fc279aac251aa220090ebea999c5ce3739e375
Kangyun Wang <kevinwang8011@hotmail.com>
2018-09-06 15:38:56 +0200

    Set base folder structure and router

---
+++ src/routes/Tuner/Guitar/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Guitar extends PureComponent {
+	render() {
+		return <div>Guitar</div>;
+	}
+}
+
+export default Guitar;
